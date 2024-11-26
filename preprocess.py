1ba9b9378316ceddaa7e2a4320193ab6f582a3e2
crowbarmassage <96141671+crowbarmassage@users.noreply.github.com>
2024-11-26 14:10:37 -0600

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -28,16 +28,16 @@ if not os.path.isdir(dir):
 print(data.num_nodes)
 idx_lcc, adj_norm_lcc, _ = get_largest_cc(data.adj_full, data.num_nodes, args.dataset)
 np.save(f"{dir}/idx_lcc.npy", idx_lcc)
-# np.save(f"{dir}/adj_norm_lcc.npy", adj_norm_lcc)
-np.save("adj_norm_lcc.npy", array.astype(np.float64)) # Switch this line to fix pickle problem
+np.save(f"{dir}/adj_norm_lcc.npy", adj_norm_lcc)
+# np.save("adj_norm_lcc.npy", array.astype(np.float64)) # Switch this line to fix pickle problem
 
 L_lcc = sp.eye(len(idx_lcc)) - adj_norm_lcc
 print("Laplacian of largest connected component shape:  ", L_lcc.shape)    
 eigenvals_lcc, eigenvecs_lcc = get_eigh(L_lcc, f"{args.dataset}", True)
 print("Minimum Laplacian eigenvalue:", min(eigenvals_lcc))
 print("Maximum Laplacian eigenvalue:", max(eigenvals_lcc))
-# np.save(f"{dir}/L_norm_lcc.npy", L_lcc)
-np.save("L_norm_lcc.npy", array.astype(np.float64)) # Switch this line to fix pickle problem
+np.save(f"{dir}/L_norm_lcc.npy", L_lcc)
+# np.save("L_norm_lcc.npy", array.astype(np.float64)) # Switch this line to fix pickle problem
 
 idx_train_lcc, idx_map = get_train_lcc(idx_lcc=idx_lcc, idx_train=data.idx_train, y_full=data.y_full, num_nodes=data.num_nodes, num_classes=data.num_classes)
 np.save(f"{dir}/idx_train_lcc.npy", idx_train_lcc)
